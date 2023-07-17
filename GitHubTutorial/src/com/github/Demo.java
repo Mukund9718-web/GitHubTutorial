524aa93f0431adedcd7be5db2887367d335bdcab
Dell <Dell@192.168.1.5>
2023-07-17 17:21:28 +0530

    hi this is my first commint

---
+++ GitHubTutorial/src/com/github/Demo.java
@@ -0,0 +1,17 @@
+package com.github;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello guys how are you ");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
